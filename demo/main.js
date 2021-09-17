35561ea1aab5765c75be46e3f09aa1029923157d
junholeec <leejh@hancom.com>
2021-09-17 15:39:22 +0900

    [Refactoring] add cr/lf in main.js
    add line space(cr/lf) in main.js

--- demo/main.js
+++ demo/main.js
@@ -1,15 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     var rightcard = false;
     var tempblock;
     var tempblock2;
+
     document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>';
+
     flowy(document.getElementById("canvas"), drag, release, snapping);
+
     function addEventListenerMulti(type, listener, capture, selector) {
         var nodes = document.querySelectorAll(selector);
         for (var i = 0; i < nodes.length; i++) {
             nodes[i].addEventListener(type, listener, capture);
         }
     }
+
     function snapping(drag, first) {
         var grab = drag.querySelector(".grabme");
         grab.parentNode.removeChild(grab);
@@ -40,16 +44,19 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         return true;
     }
+
     function drag(block) {
         block.classList.add("blockdisabled");
         tempblock2 = block;
     }
+
     function release() {
         if (tempblock2) {
             tempblock2.classList.remove("blockdisabled");
         }
     }
-    var disabledClick = function(){
+
+    var disabledClick = function () {
         document.querySelector(".navactive").classList.add("navdisabled");
         document.querySelector(".navactive").classList.remove("navactive");
         this.classList.add("navactive");
@@ -62,46 +69,53 @@ document.addEventListener("DOMContentLoaded", function(){
             document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="9"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Add new log entry</p><p class="blockdesc">Adds a new log entry to this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="10"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Update logs</p><p class="blockdesc">Edits and deletes log entries in this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="11"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Prompt an error</p><p class="blockdesc">Triggers a specified error</p>        </div></div></div>';
         }
     }
+
     addEventListenerMulti("click", disabledClick, false, ".side");
-    document.getElementById("close").addEventListener("click", function(){
-       if (rightcard) {
-           rightcard = false;
-           document.getElementById("properties").classList.remove("expanded");
-           setTimeout(function(){
-                document.getElementById("propwrap").classList.remove("itson"); 
-           }, 300);
+
+    document.getElementById("close").addEventListener("click", function () {
+        if (rightcard) {
+            rightcard = false;
+            document.getElementById("properties").classList.remove("expanded");
+            setTimeout(function () {
+                document.getElementById("propwrap").classList.remove("itson");
+            }, 300);
             tempblock.classList.remove("selectedblock");
-       } 
+        }
     });
-    
-document.getElementById("removeblock").addEventListener("click", function(){
- flowy.deleteBlocks();
-});
-var aclick = false;
-var noinfo = false;
-var beginTouch = function (event) {
-    aclick = true;
-    noinfo = false;
-    if (event.target.closest(".create-flowy")) {
-        noinfo = true;
+
+    document.getElementById("removeblock").addEventListener("click", function () {
+        flowy.deleteBlocks();
+    });
+
+    var aclick = false;
+    var noinfo = false;
+
+    var beginTouch = function (event) {
+        aclick = true;
+        noinfo = false;
+        if (event.target.closest(".create-flowy")) {
+            noinfo = true;
+        }
     }
-}
-var checkTouch = function (event) {
-    aclick = false;
-}
-var doneTouch = function (event) {
-    if (event.type === "mouseup" && aclick && !noinfo) {
-      if (!rightcard && event.target.closest(".block") && !event.target.closest(".block").classList.contains("dragging")) {
-            tempblock = event.target.closest(".block");
-            rightcard = true;
-            document.getElementById("properties").classList.add("expanded");
-            document.getElementById("propwrap").classList.add("itson");
-            tempblock.classList.add("selectedblock");
-       } 
+
+    var checkTouch = function (event) {
+        aclick = false;
     }
-}
-addEventListener("mousedown", beginTouch, false);
-addEventListener("mousemove", checkTouch, false);
-addEventListener("mouseup", doneTouch, false);
-addEventListenerMulti("touchstart", beginTouch, false, ".block");
+
+    var doneTouch = function (event) {
+        if (event.type === "mouseup" && aclick && !noinfo) {
+            if (!rightcard && event.target.closest(".block") && !event.target.closest(".block").classList.contains("dragging")) {
+                tempblock = event.target.closest(".block");
+                rightcard = true;
+                document.getElementById("properties").classList.add("expanded");
+                document.getElementById("propwrap").classList.add("itson");
+                tempblock.classList.add("selectedblock");
+            }
+        }
+    }
+
+    addEventListener("mousedown", beginTouch, false);
+    addEventListener("mousemove", checkTouch, false);
+    addEventListener("mouseup", doneTouch, false);
+    addEventListenerMulti("touchstart", beginTouch, false, ".block");
 });
